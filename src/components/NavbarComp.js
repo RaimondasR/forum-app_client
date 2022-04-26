36895f279f37fc01f186663d71cb8b2cefe74aef
RaimondasR <raimondas.r2@gmail.com>
2022-04-26 23:52:20 +0300

    Code updated. NavbarComp.js file is modified.

--- src/components/NavbarComp.js
+++ src/components/NavbarComp.js
@@ -1,35 +1,33 @@
 import React, { useContext } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { UserContext } from '../context/UserContext';
+import { MainContext } from '../context/MainContext';
 import { useNavigate } from 'react-router-dom';
-
-
 
 const NavbarComp = () => {
   const location = useLocation(); 
-  const {getUser, setUser} = useContext(UserContext);
-  const nav = useNavigate();  
+  const nav = useNavigate();
+  const { loggedInUser, setLoggedInUser } = useContext(MainContext);  
 
   function SignOutUser() {
     nav("/home");
-    setUser(null);
+    setLoggedInUser(null);
     
   }
 
   return (
     <div className="NavbarComp d-flex row">
         {/* if user neither registered, nor logged in, then "login" and "register" links are shown */}
-        {(!getUser && location.pathname !=="/login") && <Link to="/login">Sign In</Link>}
-        {(!getUser && location.pathname !=="/register") && <Link to="/register">New User? Sign Up</Link>}   
+        {(!loggedInUser && location.pathname !=="/login") && <Link to="/login">Login</Link>}
+        {(!loggedInUser && location.pathname !=="/register") && <Link to="/register">New User? Sign Up</Link>}   
         {location.pathname !=="/home" && <Link to="/home">Home</Link>}         
-        {(getUser && location.pathname !=="/create-topic") && <Link to="/create-topic">Start new topic</Link>}
+        {(loggedInUser && location.pathname !=="/create-topic") && <Link to="/create-topic">Start new topic</Link>}
         {location.pathname !=="/all-topics" && <Link to="/all-topics">Topics</Link>}
         
-        {getUser && 
+        { loggedInUser && 
           <div className="userStats d-flex row">
-            <div className="mr40">Username: {/*getUser.username*/}            
+            <div className="mr40">Username: {loggedInUser.userName}            
             </div>
-            <div onClick={SignOutUser}>Sign Out {/* setUser(null) */}
+            <div onClick={SignOutUser}>Sign Out
             </div>
           </div>
         }
