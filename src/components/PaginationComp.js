ab37a1b6b725cf3096926374e8f9dad12f7fe704
RaimondasR <raimondas.r2@gmail.com>
2022-04-27 18:59:43 +0300

    Modified App.css, PaginationComp.js, SingleTopicComp.js files.

--- src/components/PaginationComp.js
+++ src/components/PaginationComp.js
@@ -1,9 +1,73 @@
 import React from 'react';
+import styles from "../App.css";
 
-const PaginationComp = () => {
-  return (
-    <div>PaginationComp</div>
-  )
+const PaginationComp = ({page, commentsCount, turnPage}) => {
+  function doPagination() {
+    if (commentsCount <= 10) {
+      return (
+        <div className="pagination-div">Page: 1</div>
+      )
+    }
+    if (commentsCount > 10) {
+      let counter = commentsCount / 10;
+      let remainder = commentsCount % 10;
+      let arr = [];
+
+      if (remainder === 0) {
+        for (let i = 0; i < counter; i++) {
+            arr.push(i+1)
+        }  
+      }
+      if (remainder !== 0) {
+        for (let i = 0; i < counter; i++) {
+          arr.push(i+1)
+        }  
+      }
+
+      return (
+        <div className="pagination-div">
+          <div className="d-flex">
+            <div className="changePrevPage-div"
+                 onClick={() => changePreviousPage()}
+            >Previous</div>
+            {arr.map(x => <div key={x} className={ page === x ? 
+                                       `${styles.border-tbr} ${styles.hover} ${styles.bg-light}` 
+                                       : 
+                                       `${styles.border-tbr} ${styles.hover}`}
+                                       onClick={() => changeCustomPage(x)}
+            >{x}</div>)}
+            <div className={`${styles.nextPage} ${styles.hover}`}
+                  onClick={() => changeNextPage(arr)}  
+            >Next</div>
+          </div>
+        </div>
+        )             
+    }
+}
+
+function changeCustomPage(clickedPage) {
+  if (clickedPage !== page) {
+      turnPage(clickedPage)
+  }
+}
+
+function changeNextPage(arrayOfPages) {
+  if (page < arrayOfPages[arrayOfPages.length-1]) {
+      turnPage(page + 1);
+  }       
+}
+
+function changePreviousPage() {
+  if (page > 1) {
+      turnPage(page - 1);
+  }
+}
+
+return (
+  <div>
+      { doPagination() }
+  </div>)
+
 }
 
 export default PaginationComp;
--- src/components/SingleTopicComp.js
+++ src/components/SingleTopicComp.js
@@ -11,7 +11,7 @@ const SingleTopicComp = () => {
   const [topic, setTopic] = useState(null);
   const [comments, setComments] = useState(null);
   const [page, setPage] = useState(1);
-  const [count, setCount] = useState(null);
+  const [commentsCount, setCommentsCount] = useState(null);
 
   useEffect(() => {
     http.get(`getSingleTopic/${id}`)
@@ -25,7 +25,7 @@ const SingleTopicComp = () => {
         .then((res) => {
           if  (res.success) {
             setComments(res.comments);
-            setCount(res.count);
+            setCommentsCount(res.commentsCount);
           }
         })
   }, [])
@@ -37,7 +37,7 @@ const SingleTopicComp = () => {
         .then((res) => {
             if  (res.success) {
                 setComments(res.comments);
-                setCount(res.count);
+                setCommentsCount(res.commentsCount);
             }
         })
   } // end of function turnPage()
@@ -46,10 +46,10 @@ const SingleTopicComp = () => {
     return (
       <div className="mapComments">
         {comments.map((x, i) => <div key={x.commentId}><SingleCommentComp profile={false} index={i} page={page} comment={x}/></div>)}
-        <PaginationComp page={page} count={count} turnPage={turnPage}/>
+        <PaginationComp page={page} commentsCount={commentsCount} turnPage={turnPage}/>
       </div>
     )
-  }
+  } // end of function mapComments()
 
 
   return (
@@ -57,9 +57,9 @@ const SingleTopicComp = () => {
       {topic && comments && 
         <div className="SingeTopicComp d-flex">
             <TitleComp title={topic.topicTitle}/>
-            <PaginationComp page={page} count={count} turnPage={turnPage}/>
+            <PaginationComp page={page} commentsCount={commentsCount} turnPage={turnPage}/>
             { mapComments(comments) }
-            <AddCommentComp topicId={id} setComments={setComments} commentCount={count} setPage={setPage} setCount={setCount}/>
+            <AddCommentComp topicId={id} setComments={setComments} commentsCount={commentsCount} setPage={setPage} setCommentsCount={setCommentsCount}/>
         </div>
       }            
     </div>
